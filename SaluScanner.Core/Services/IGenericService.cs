a5105d20267e9bd29693d489322bc993cdb12b7b
Deniz Demirkan <denizdemirkan.drive@gmail.com>
2022-11-17 14:16:51 +0300

    Repository Layer Initialized

---
+++ SaluScanner.Core/Services/IGenericService.cs
@@ -0,0 +1,24 @@
+﻿using SaluScanner.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaluScanner.Core.Services
+{
+    public interface IGenericService<TEntity> where TEntity : class, IEntity, new()
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> expression);
+        Task<TEntity> AddAsync(TEntity entity);
+        Task AddRangeAsyn(IEnumerable<TEntity> entities);
+        Task UpdateAsync(TEntity entity);
+        Task RemoveAsync(TEntity entity);
+        Task RemoveRangeAsync(IEnumerable<TEntity> entities);
+    }
+}
